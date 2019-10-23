63c6e003322dfa657993a7f4517f9cbb9b09a099
fugroup <vidar@fugroup.net>
2019-10-23 13:13:59 +0200

    Update

--- README.md
+++ README.md
@@ -1,9 +1,62 @@
 # BRSRKR
-Tiny Javascript HTML and DOM manipulation library, jQuery style
+Tiny Javascript HTML and DOM manipulation library, encapsulation style.
 
 ### Install
 `npm i brsrkr`
 
-See the tests for how to use it.
+### Usage
+```javascript
+// Get HTML
+$('#app').html()
 
+// Insert HTML
+$('#app').html('<div>Before</div>')
+
+// Insert HTML before element
+$('#app').html('<div>hello</div>', 'before')
+
+// Insert HTML after element
+$('#app').html('<div>hello</div>', 'after')
+
+// Insert HTML at top of element
+$('#app').html('<div>hello</div>', 'top')
+
+// Insert HTML at end of element
+$('#app').html('<div>hello</div>', 'end')
+
+// Get text
+$('#app').text()
+
+// Insert text
+$('#app').text('hello')
+
+// Query DOM, uses callback
+$('#app', el => el.textContent = 'hello')
+
+// Function body applies to all matches
+$('li', el => {
+  el.style.color = 'yellow'
+  el.textContent = 'hello'
+
+  // Can use brsrkr here too
+  $(el).html('<span>Nice!</span>')
+
+  // Query only inside el
+  $('span', el, span => {
+    span.style.display = 'none'
+  })
+})
+
+// Find all span inside '#el'
+$('span', '#el', span => {
+  span.style.opacity = 1
+})
+
+// Get by index
+$('li').get(0)
+
+// Get number of matches
+$('li').length
+
+```
 ISC licensed. Enjoy!
--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-function h(s, h, x) {
-  return !h && q(s).innerHTML ||
-    !x && (q(s).innerHTML = h) ||
-    x[0] == 'r' && (q(s).outerHTML = h) ||
-    q(s).insertAdjacentHTML(
-      x[0] == 'b' && 'beforebegin' ||
-      x[0] == 'a' && 'afterend' ||
-      x[0] == 't' && 'afterbegin' ||
-      x[0] == 'e' && 'beforeend', h)
+function $(s, r, fn) {
+  if (typeof r == 'function') { fn = r; r = undefined }
+  r = typeof r == 'string' ? document.querySelector(r) : r || document
+  s = typeof s == 'string' ? r.querySelectorAll(s) : s
+  if (s instanceof Element || s instanceof HTMLDocument) s = [s]
+  if (typeof fn == 'function' && s.length)
+    for (let i = 0; i < s.length; i++) fn(s[i])
+  return {
+    get(n) {
+      return Number.isInteger(n) ? s[n] : s
+    },
+    get length() {
+      return s.length
+    },
+    html(str, mod) {
+      if (str) {
+        for (let i = 0; i < s.length; i++) {
+          !mod && (s[i].innerHTML = str) ||
+          mod[0] == 'r' && (s[i].outerHTML = str) ||
+          s[0].insertAdjacentHTML(
+            mod[0] == 'b' && 'beforebegin' ||
+            mod[0] == 'a' && 'afterend' ||
+            mod[0] == 't' && 'afterbegin' ||
+            mod[0] == 'e' && 'beforeend', str)
+        }
+        return this
+      }
+      return s[0] && s[0].innerHTML
+    },
+    text(str) {
+      if (str) {
+        for (let i = 0; i < s.length; i++)
+          s[i].textContent = str
+        return this
+      }
+      return s[0].textContent
+    }
+  }
 }
-
-function t(s, t) {
-  return t ? (q(s).textContent = t) : q(s).textContent
-}
-
-function q(s, f) {
-  return typeof s == 'string' ? (f ? q(f) : document).querySelector(s) : s
-}
-
-function qa(s, f) {
-  return (f ? q(f) : document).querySelectorAll(s)
-}
-
-function m(t, ...d) {
-  for (var h = '', i = 0; i < d.length; i++) h += t[i] + d[i]
-  return h += t[i]
-}
-
-module.exports = { h, t, q, qa, m }
+module.exports = $
